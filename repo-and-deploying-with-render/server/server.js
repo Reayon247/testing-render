21ea1f729ba9002ba12cab015dbabd366cfacb59
Ryan Nugent <ryan.nugent247@gmail.com>
2025-05-19 16:19:24 +0100

    added get

--- repo-and-deploying-with-render/server/server.js
+++ repo-and-deploying-with-render/server/server.js
@@ -7,3 +7,7 @@ const app = express();
 app.listen(8080, function () {
   console.log("Server is listening on port 8080...");
 });
+
+app.get("/messages", function (request, response) {
+  response.json({ message: "Hello, World!" });
+});
