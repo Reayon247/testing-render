2188ad5951b965a7f36cdad9e7edb6d99022b69b
Ryan Nugent <ryan.nugent247@gmail.com>
2025-05-19 16:14:26 +0100

    added server.js

---
+++ repo-and-deploying-with-render/server/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+// your routes will go here...
+
+app.listen(8080, function () {
+  console.log("Server is listening on port 8080...");
+});
